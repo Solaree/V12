2cc076200bff9bff86cf6bf8f047d9040b158719
Solar <115794865+Solaree@users.noreply.github.com>
2023-01-03 20:07:21 +0200

    Delete Protocol directory

--- Protocol/LogicCommandManager.py
+++
@@ -1,9 +0,0 @@
-from Protocol.Commands.Client.LogicGatchaCommand import LogicGatchaCommand
-from Protocol.Commands.Client.LogicPurchaseOfferCommand import LogicPurchaseOfferCommand
-from Protocol.Commands.Client.ChangeIconCommand import ChangeIconCommand
-
-commands = {
-    500: LogicGatchaCommand,
-    505: ChangeIconCommand,
-    519: LogicPurchaseOfferCommand,
-}
--- Protocol/ServerCommandsFactory.py
+++
@@ -1,7 +0,0 @@
-from Protocol.Commands.Server.LogicGiveDeliveryItemsCommand import LogicGiveDeliveryItemsCommand
-from Protocol.Commands.Server.LogicDiamondsAddedCommand import LogicDiamondsAddedCommand
-
-commands = {
-    203: LogicGiveDeliveryItemsCommand,
-    202: LogicDiamondsAddedCommand,
-}
